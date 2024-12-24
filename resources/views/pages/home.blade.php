0b72b466b4f208711bda57b360e42af9ecfc5c56
jinaneizrafil <izrafiljinane@gmail.com>
2024-12-24 16:43:38 +0200

    frontend setup

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('head')
+    <title>home</title>
+@endsection
+
+@section('content')
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+@endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            screens: {
+                xs: "320px",
+                sm: "640px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1280px",
+                xxl: "1536px",
+                xxxl: "1950px",
             },
         },
     },

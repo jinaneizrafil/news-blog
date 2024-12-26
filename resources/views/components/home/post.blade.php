9b1fa6f8fa893e058ec7ab2e02fd1c1eed20b30b
jinaneizrafil <izrafiljinane@gmail.com>
2024-12-26 18:30:06 +0200

    responsive

--- resources/views/components/home/post.blade.php
+++ resources/views/components/home/post.blade.php
@@ -2,9 +2,9 @@
     class="post p-2 col-span-4 bg-gray-100 rounded hover:bg-gray-200 hover:shadow-md focus:ring focus:ring-blue-300 transition">
     <a href="{{ route('post.show', $post->slug) }}">
         <div class="flex flex-col items-center pb-7">
-            <div class="text hover:underline font-semibold flex text-center">{{ $post->title }}</div>
+            <div class="text hover:underline font-semibold flex text-center text-[16px] md:text-[18px]">{{ $post->title }}</div>
 
-            <div class="sub-title flex text-gray-500 pb-7">
+            <div class="sub-title flex text-gray-500 pb-7 text-[10px] md:text-[13px]">
 
                 {{ $post->date }}|{{ $category->name }}
 
@@ -12,8 +12,6 @@
             <img src="{{ url('storage/' . $post->image) }}">
 
             {{-- <div class="content pl-8">{{ $post->content }}</div> --}}
-
         </div>
-
     </a>
 </div>
